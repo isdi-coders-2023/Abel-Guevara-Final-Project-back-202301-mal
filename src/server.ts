942a2ffc99e5427d31a6161ef87123fd0d5024b6
Abel <abelguevaralanda@yahoo.es>
2023-03-14 17:48:53 +0100

    create connection with Mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import app from './app.js';
+import connectDB from './database/conecction.js';
 import log from './logger.js';
 
 const port = process.env.PORT ?? 3000;
+const mongoUrl = process.env.MONGO_URL ?? '';
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await connectDB(mongoUrl);
   log.info(`Server started in port ${port}`);
 });
