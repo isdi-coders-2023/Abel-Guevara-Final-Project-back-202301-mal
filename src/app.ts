4ca0bdc0e0f2fd57ff26172190be6043dc52b2f3
Abel <abelguevaralanda@yahoo.es>
2023-03-15 17:39:41 +0100

    fix cors for connect with fron

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import authRouter from './api/auth/auth-router.js';
+import { errorHandler } from './utils/error-handler.js';
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+
 app.get('/', (_req, res) => {
   res.json({ msg: 'My server is up' });
 });
+app.use(cors());
 
 app.use(express.json());
+
+app.disable('x-powered-by');
+
 app.use('/auth', authRouter);
+
+app.use(errorHandler);
 
 export default app;
