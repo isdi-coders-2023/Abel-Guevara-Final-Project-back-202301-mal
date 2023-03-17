c9f57b0cc06d7c0086f58291229b818e57409109
Abel <abelguevaralanda@yahoo.es>
2023-03-17 12:28:48 +0100

    add auth register developed

--- src/api/auth/auth-controller.ts
+++ src/api/auth/auth-controller.ts
@@ -1,13 +1,37 @@
 import { RequestHandler } from 'express';
+import { CustomHTTPError } from '../../utils/custom-http-error.js';
 import { UserModel } from '../users/user-schema.js';
 import { AuthRequest, LoginResponse } from './auth-types.js';
 import { encryptPassword, generateJWTToken } from './auth-utils.js';
+
+export const registerUserController: RequestHandler<
+  unknown,
+  unknown,
+  AuthRequest
+> = async (req, res, next) => {
+  const { name, surname, email, password } = req.body;
+
+  const existingDbUser = await UserModel.findOne({ email }).exec();
+  if (existingDbUser !== null) {
+    return next(new CustomHTTPError(409, 'Su usuario ya está registrado'));
+  }
+
+  const user = {
+    name,
+    surname,
+    email,
+    password: encryptPassword(password),
+  };
+
+  await UserModel.create(user);
+  res.sendStatus(201);
+};
 
 export const loginUserController: RequestHandler<
   unknown,
   LoginResponse,
   AuthRequest
-> = async (req, res) => {
+> = async (req, res, next) => {
   const { email, password } = req.body;
   const filterUser = {
     email,
@@ -17,7 +41,7 @@ export const loginUserController: RequestHandler<
   const existingUser = await UserModel.findOne(filterUser).exec();
 
   if (existingUser === null) {
-    return res.sendStatus(404);
+    return next(new CustomHTTPError(404, 'Su correo o contraseña no existe'));
   }
 
   const tokenJWT = generateJWTToken(email);
--- src/api/auth/auth-router.test.ts
+++ src/api/auth/auth-router.test.ts
@@ -5,6 +5,7 @@ import connectDB from '../../database/conecction';
 import app from '../../app';
 import { UserModel } from '../users/user-schema';
 import { encryptPassword } from './auth-utils';
+import { AuthRequest } from './auth-types';
 describe('Given an app with auth-router', () => {
   let mongoServer: MongoMemoryServer;
 
@@ -39,6 +40,51 @@ describe('Given an app with auth-router', () => {
         password: '1234',
       };
       await request(app).post('/auth/login').send(notExistUser).expect(404);
+    });
+  });
+
+  describe('When a user wants to register with a valid data', () => {
+    test('Then it should be registered', async () => {
+      const user: AuthRequest = {
+        name: 'Abelito',
+        surname: 'Guevara',
+        email: 'abelito86@gmail.com',
+        password: 'abelito1234',
+      };
+
+      await request(app).post('/auth/register').send(user).expect(201);
+    });
+  });
+
+  describe('When the user wants to register with an existing email address', () => {
+    test('Then it should receive a 409 error', async () => {
+      const user: AuthRequest = {
+        name: 'Abelito',
+        surname: 'Guevara',
+        email: 'abelito@gmail.com',
+        password: 'abelito1234',
+      };
+      await request(app).post('/auth/register').send(user).expect(409);
+    });
+  });
+
+  describe('When a user wants to register with an invalid email', () => {
+    test('Then it should not be registered', async () => {
+      const invalidUser: AuthRequest = {
+        name: 'Abelito',
+        surname: 'Guevara',
+        email: 'm@',
+        password: 'abelito1234',
+      };
+
+      const response = await request(app)
+        .post('/auth/register')
+        .send(invalidUser)
+        .expect(400);
+
+      expect(response.body).toEqual({
+        msg: '"email" must be a valid email',
+      });
     });
   });
 });
