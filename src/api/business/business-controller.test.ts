dc56cd325724e1e1107cf48ebd2cd335af95a4cc
Abel <abelguevaralanda@yahoo.es>
2023-03-22 19:56:09 +0100

    add get all business controller

--- src/api/business/business-controller.test.ts
+++ src/api/business/business-controller.test.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
+import { CustomHTTPError } from '../../utils/custom-http-error';
 import { BusinessModel } from './busines-model';
-import { createBusinessController } from './business-controller';
+import {
+  createBusinessController,
+  getBusinessController,
+} from './business-controller';
 
-describe('Given a business controller', () => {
+describe('Given a create business controller', () => {
   const request = {
     body: {
       categories: 'barberia',
@@ -25,7 +29,7 @@ describe('Given a business controller', () => {
   } as Partial<Response>;
 
   describe('When the user wants to register his business', () => {
-    test.only('Then he should do it', async () => {
+    test('Then he should do it', async () => {
       BusinessModel.create = jest.fn().mockResolvedValue(request);
 
       await createBusinessController(
@@ -38,6 +42,52 @@ describe('Given a business controller', () => {
         ...request.body,
       });
       expect(response.status).toHaveBeenCalledWith(201);
+    });
+  });
+});
+
+describe('Given a get business controller', () => {
+  const request = {} as Request;
+  const response = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as Partial<Response>;
+  const next = jest.fn();
+
+  const business = {
+    _id: 'mockId',
+    categories: 'barberia',
+    nameBusiness: 'barber',
+    address: 'malaga',
+    phone: '123456789',
+    profileUrl: 'mockPicture.jpg.com',
+    description: 'la mejor',
+    reviews: ['muy bien atendido'],
+    score: [5],
+  };
+  describe('When the user wants get all business', () => {
+    test('Then should get all business', async () => {
+      BusinessModel.find = jest.fn().mockImplementation(() => ({
+        exec: jest.fn().mockResolvedValue(business),
+      }));
+
+      await getBusinessController(request, response as Response, next);
+
+      expect(response.json).toHaveBeenCalledWith(business);
+      expect(BusinessModel.find).toHaveBeenCalledWith({}, { __v: 0 });
+    });
+  });
+  describe('When dont exist business', () => {
+    test('Then the user should recive a 404 error', async () => {
+      BusinessModel.find = jest.fn().mockImplementation(() => ({
+        exec: jest.fn().mockResolvedValue(null),
+      }));
+
+      await getBusinessController(request, response as Response, next);
+
+      expect(next).toHaveBeenCalledWith(
+        new CustomHTTPError(404, 'Lo sentimos, no hay salones que mostrar'),
+      );
     });
   });
 });
